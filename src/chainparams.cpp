010adf4603765bdcca1ce8f496775ee1d86c1ac0
Wesley Menezes <w.menezes@loro.com.co>
2021-07-20 10:47:48 -0300

    Update: nodes gerais interno

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -183,6 +183,7 @@ public:
         vSeeds.push_back(CDNSSeedData("node1.cashcash.com.br","node2.cashcash.com.br"));
         vSeeds.push_back(CDNSSeedData("node3.cashcash.com.br","node4.cashcash.com.br"));
         vSeeds.push_back(CDNSSeedData("node5.cashcash.com.br","node6.cashcash.com.br"));
+        vSeeds.push_back(CDNSSeedData("node7.cashcash.com.br","node1.walletbuilders.com"));
         
         base58Prefixes[PUBKEY_ADDRESS] = std::vector<unsigned char>(1, 28);
         base58Prefixes[SCRIPT_ADDRESS] = std::vector<unsigned char>(1, 29);
